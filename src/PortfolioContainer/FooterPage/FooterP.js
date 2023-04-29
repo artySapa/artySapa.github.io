181d3896d8a095d396f49f155052ac191cdf38f2
Artyom Sapa <artem.sapa@gmail.com>
2023-04-28 23:26:03 -0700

    slight change for the footer

--- src/PortfolioContainer/FooterPage/FooterP.js
+++ src/PortfolioContainer/FooterPage/FooterP.js
@@ -1,31 +1,42 @@
-import { React } from "react";
-import "./FooterP.css";
+import React from 'react';
 
-export default function FooterP() {
-  return (
-    <div className="footer">
-      <div class="footerEntry">
-        <p className="footerLink">
-          <a
-            href="https://www.instagram.com/sapartyom/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <i className="fa fa-instagram"></i>
-          </a>
-        </p>
-        <p className="footerLink">
-          <a
-            href="https://www.linkedin.com/in/sapartyom/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <i className="fa fa-linkedin"></i>
-          </a>
-        </p>
-        <p className="footerLink">sapartyom@g.ucla.edu</p>
-        <p className="footerLink">(424)415-2827</p>
+import './FooterP.css'
+
+export default function Footer(props) {
+    return (
+        <div className="footer">
+        <div class="footerEntry">
+          <p className="footerLink">
+            <a
+              href="https://www.instagram.com/sapartyom/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i className="fa fa-instagram"></i>
+            </a>
+          </p>
+          <p className="footerLink">
+            <a
+              href="https://github.com/artySapa"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i className="fa fa-github"></i>
+            </a>
+          </p>
+          <p className="footerLink">
+            <a
+              href="https://www.linkedin.com/in/sapartyom/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i className="fa fa-linkedin"></i>
+            </a>
+          </p>
+          <p className="footerLink">sapartyom@g.ucla.edu</p>
+          <p className="footerLink">(424)415-2827</p>
+          <a className="footerLink" href="./artycv.pdf" download="Arty artycv.pdf">Resume</a>
+        </div>
       </div>
-    </div>
-  );
+    );
 }
